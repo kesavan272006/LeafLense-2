2f902323972fae943aae7215c6786a0c13a04471
Pranav C R <pranavcr001@gmail.com>
2025-08-25 03:09:22 +0530

    make pwa

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { Workbox } from 'workbox-window'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,3 +12,9 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </StrictMode>,
 )
+
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  const wb = new Workbox('/sw.js')
+  wb.register()
+}
